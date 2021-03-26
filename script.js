4f53c779c0b6e7cd15869d62a0ef9851d460ef62
markeezy <markkhoo95@gmail.com>
2021-03-26 14:01:52 -0700

    add few lines of js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Pointers
+
+
+// Variables - Global Scope
+var wordBank = ["Like","Comment","Subscribe","Share","Follow","Create"];
+
+
+// Keyboard UP - Listener
+function keyupAction (event){
+    console.log(event.key);
+};
+
+document.addEventListener("keyup", keyupAction);
+
+
+// Randomly Select Word
